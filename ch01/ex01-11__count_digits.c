29428cc06c71d02c57af26479f0c742269b7bf91
Tyler Carbone <tcarbone073@gmail.com>
2021-06-08 22:11:35 +0000

    finish comments

--- ch01/ex01-11__count_digits.c
+++ ch01/ex01-11__count_digits.c
@@ -11,11 +11,17 @@ int main(void) {
     nwhite = nother = 0;
     for (i = 0; i < 10; ++i)
         ndigit[i] = 0;
-
-    // 
+ 
     while ((c = getchar()) != EOF)
+
+        /* here, we test if the value of the character is between 30 and 39
+         * (i.e., ascii values for 0 thru 9). this is implied becuase c is
+         * declared to be an int. the value of c minus the ascii value for 0
+         * (30) yields the value of the digit. then, its place in the array is
+         * increased by one. */
         if (c >= '0' && c <= '9')
             ++ndigit[c-'0'];
+
         else if (c == ' ' || c == '\n' || c == '\t')
             ++nwhite;
         else
