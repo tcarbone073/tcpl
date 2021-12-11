f685cc0b4c225ae22aa6663f9e602899d9fc8656
Tyler Carbone <tcarbone@LinuxBox.home>
2021-12-11 11:20:03 -0500

    Exercise 02-01: print <limit.h> constants

--- ch02/ex02-01.c
+++ ch02/ex02-01.c
@@ -7,11 +7,35 @@
 #include<limits.h>
 #include<float.h>
 
-int main(void) {
+void disp(char type_name[], long size, char desc[]);
 
-	printf("  %s\t%d %s\n", "CHAR_BIT", CHAR_BIT, "bits in a 'CHAR'");
-	printf("  %s\t%d %s\n", "CHAR_MAX", CHAR_MAX, "max value of a 'CHAR'");
-	printf("  %s\t%d %s\n", "CHAR_MIN", CHAR_MIN, "min value of a 'CHAR'");
+int main(void) {
+	
+	printf("\n");
+	disp("CHAR_BIT",  CHAR_BIT,  "bits in a 'char'");
+	disp("CHAR_MIN",  CHAR_MIN,  "minimum value of 'char'");
+	disp("CHAR_MAX",  CHAR_MAX,  "maximum value of 'char'");
+	disp("SCHAR_MIN", SCHAR_MIN, "minimum vlaue of 'signed char'");
+	disp("SCHAR_MAX", SCHAR_MAX, "maximum value of 'signed char'");
+	disp("UCHAR_MAX", UCHAR_MAX, "maximum value of 'unsigned char'");
+	disp("INT_MIN",   INT_MIN,   "minimum value of 'int'");
+	disp("INT_MAX",   INT_MAX,   "maximum value of 'int'");	
+	disp("UINT_MAX",  UINT_MAX,  "maximum value of 'unsigned int'");
+	disp("LONG_MIN",  LONG_MIN,  "minimum value of 'long int'");
+	disp("LONG_MAX",  LONG_MAX,  "maximum value of 'long int'");
+	disp("ULONG_MAX", ULONG_MAX, "maximum value of 'unsigned long'");
+	disp("SHRT_MIN",  SHRT_MIN,  "minimum value of 'short int'");
+	disp("SHRT_MAX",  SHRT_MAX,  "maximum value of 'short int'");
+	disp("USHRT_MAX", USHRT_MAX, "maximum value of 'unsigned short int'");
+	printf("\n");
 
 	return 0;
+}
+
+void disp(char type_name[], long size, char desc[]) {
+	/* Print as unsigned or signed. */
+	if (type_name[0] == 'U')
+		printf("%12s%22lu  %s\n", type_name, size, desc);
+	else
+		printf("%12s%22ld  %s\n", type_name, size, desc);
 }
